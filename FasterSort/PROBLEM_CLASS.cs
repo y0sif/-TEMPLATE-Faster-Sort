9cf2c01543a88a21d4c265311f59f1b1ca18514e
Yosif Soliman <yosifsoli981@gmail.com>
2024-03-06 22:11:09 +0200

    bubble sort

--- FasterSort/PROBLEM_CLASS.cs
+++ FasterSort/PROBLEM_CLASS.cs
@@ -26,7 +26,21 @@ namespace Problem
         static public float[] RequiredFuntion(float[] arr, int N)
         {
             //REMOVE THIS LINE BEFORE START CODING
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            float temp;
+            for(int i  = 0; i < N; i++)
+            {   
+                for (int j = 0; j < N - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            return arr;
         }
         #endregion
     }
