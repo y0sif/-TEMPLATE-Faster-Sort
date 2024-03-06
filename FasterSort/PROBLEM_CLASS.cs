386bd87c3b64350f4ed76889b96179b6116f28f2
Yosif Soliman <yosifsoli981@gmail.com>
2024-03-06 21:57:55 +0200

    Add project files.

---
+++ FasterSort/PROBLEM_CLASS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class PROBLEM_CLASS
+    {
+        #region YOUR CODE IS HERE 
+
+               //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Sort the given array in ascending order
+        /// At least, should beat the default sorting algorithm of the C# (Array.Sort())
+        /// </summary>
+        /// <param name="arr"> array to be sorted in ascending order </param>
+        /// <param name="N"> array size </param>
+        /// <returns> sorted array </returns>
+        static public float[] RequiredFuntion(float[] arr, int N)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
