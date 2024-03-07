6c775b66e3a51686e47e49b020167c7cc5a41e16
Yosif Soliman <yosifsoli981@gmail.com>
2024-03-07 14:59:24 +0200

    finished merge sort

--- FasterSort/PROBLEM_CLASS.cs
+++ FasterSort/PROBLEM_CLASS.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,7 +29,7 @@ namespace Problem
         {
             //REMOVE THIS LINE BEFORE START CODING
             //throw new NotImplementedException();
-            MergeSort(arr, 0, N);
+            MergeSort(arr, 0, N-1);
             return arr;
         }
         #endregion
@@ -39,52 +40,63 @@ namespace Problem
             {
                 int mid = (start + end) / 2;
                 MergeSort(arr, start, mid);
-                MergeSort(arr, mid, end);
+                MergeSort(arr, mid+1, end);
                 Merge(arr, start, mid, end);
             }
         }
 
         static private void Merge(float[] arr, int start, int mid, int end)
         {
-            //Console.WriteLine("in merge");
-            int n1 = mid - start+1;
+            int n1 = mid - start + 1;
             int n2 = end - mid;
 
-            float[] arr1 = new float[n1 + 1];
-            float[] arr2 = new float[n2 + 1];
+            float[] arr1 = new float[n1];
+            float[] arr2 = new float[n2];
 
-            //Console.WriteLine("in first arr");
-            for (int k = 0; k < n1; k++)
-            {
-                //Console.WriteLine(arr[start + k]);
-                arr1[k] = arr[start + k];
-            }
+            for (int e = 0; e < n1; e++)
+                arr1[e] = arr[start + e];
 
-            //Console.WriteLine("in second arr");
-            for (int k = 0; k < n2; k++)
-            {
-                //Console.WriteLine(arr[k + mid]);
-                arr2[k] = arr[k + mid];
-            }
+            for (int e = 0; e < n2; e++)
+                arr2[e] = arr[mid + 1 + e];
 
-            arr1[n1] = arr2[n2] = float.PositiveInfinity;
             int i = 0;
             int j = 0;
-            for (int k = start; k < end; k++)
+            int k = start;
+            for ( ; k < end; k++) 
             {
-                //Console.WriteLine("arr1: "+arr1[i]);
-                //Console.WriteLine("arr2: "+ arr2[j]);
-                if (arr1[i] <= arr2[j])
+                if (i < n1 && j < n2)
                 {
-                    arr[k] = arr1[i];
-                    i++;
+                    if (arr1[i] <= arr2[j])
+                    {
+                        arr[k] = arr1[i];
+                        i++;
+                    }
+                    else
+                    {
+                        arr[k] = arr2[j];
+                        j++;
+                    }
                 }
                 else
                 {
-                    arr[k] = arr2[j];
-                    j++;
+                    break;
                 }
             }
+
+            while (i < n1)
+            {
+                arr[k] = arr1[i];
+                k++;
+                i++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = arr2[j];
+                k++;
+                j++;
+            }
+
         }
     }
 }
